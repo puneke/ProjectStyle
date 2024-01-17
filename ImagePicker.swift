b859e9b42a35560ebb96529f5c40b34d372e7be2
puneke <afogeater@gmail.com>
2024-01-17 21:05:28 +1300

    created imagepicker object

--- ImagePicker.swift
+++ ImagePicker.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: Image?
+    @Environment(\.presentationMode) var presentationMode
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        // This function is required but you don't need to implement anything here for now.
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = Image(uiImage: uiImage)
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
--- StyleSwipe/ContentView.swift
+++ StyleSwipe/ContentView.swift
@@ -7,17 +7,39 @@
 
 import SwiftUI
 import CoreData
+import UIKit
 
 struct ContentView: View {
+    @State private var isShowingImagePicker = false
+    @State private var image: Image?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.blue)
-            Text("Project Updated")
+            Text("Style Swipe")
+                .font(Font.custom("Futura", size:35))
+                .padding()
+
+            if let image = image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            }
+
+
+            Button("Upload Photo") {
+                isShowingImagePicker = true
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(10)
+        }
+        .sheet(isPresented: $isShowingImagePicker) {
+            ImagePicker(image: $image)
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
